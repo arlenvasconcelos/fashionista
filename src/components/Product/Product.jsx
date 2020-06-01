5280312a0f1cd1d46b86331afa7cd2f0d8bdb013
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-05-31 21:54:49 -0300

    create noimg on product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -7,16 +7,22 @@ const Product = ({product}) => {
   return (
     <>
       <div className="product">
-        <Link to={`/produto/${product.name.replace(' ', '-')}`}>
-          <div>
-            <img src={product.image} alt={product.name}/>
-          </div>
-          <div className="product__footer">
-            <div className="product__name">
-              {product.name}
-            </div>
-            <div>
-              {product.actual_price}  
+        <Link 
+          to={`/produto/${product.name.toLowerCase().trim().replace(/ /g, '-')}`}
+        >
+          <div className="product__content">
+            {
+              product.image 
+                ? <img src={product.image} alt={product.name}/>
+                : <div className="product__noimage">Imagem indisponível</div>
+            }
+            <div className="product__footer">
+              <div className="product__name">
+                {product.name}
+              </div>
+              <div>
+                {product.actual_price}  
+              </div>
             </div>
           </div>
         </Link>
