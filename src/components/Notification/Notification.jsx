1854cec388857685088f5f33cdd76e7ba4ac0be0
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-06-04 14:40:46 -0300

    create notifcation component

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Notification.scss';
+
+const Notification = ({value}) => {
+  return (
+    <div className="notification">
+      {value}
+    </div>
+  )
+}
+
+export default Notification;
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,27 +1,51 @@
-import React from 'react';
-import {Search, ShoppingCart} from 'react-feather'
-
-import './TopBar.scss'
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import {Search, ShoppingCart} from 'react-feather';
+import {useSelector} from 'react-redux'
 
-export default ({closeCart}) => (
-  <header className="topbar">
-    <div className="container">
-      <div className="topbar__content">
-        <Link to="/">
-          <div className="topbar__logo">
-            FASHIONISTA
+import Notification from '../Notification';
+
+import './TopBar.scss';
+
+export default ({closeCart}) => {
+
+  const {cart} = useSelector(state => state);
+
+  const [quantity, setQuantity] = useState(0)
+
+  useEffect(() => {
+    setQuantity(
+      cart.items.reduce((acc, item) => {
+        return acc + item.quantity
+      }, 0)
+    );
+    
+  },[cart])
+
+  return (
+    <header className="topbar">
+      <div className="container">
+        <div className="topbar__content">
+          <Link to="/">
+            <div className="topbar__logo">
+              FASHIONISTA
+            </div>
+          </Link>
+          <div className="topbar__icons">
+            <button className="icon" >
+              <Search/>
+            </button>
+            <button className="icon" onClick={closeCart}>
+              {
+                cart.items.length 
+                  ? <Notification value={quantity}/> 
+                  : <></>
+              }
+              <ShoppingCart/>
+            </button>
           </div>
-        </Link>
-        <div className="topbar__icons">
-          <button className="icon" >
-            <Search/>
-          </button>
-          <button className="icon" onClick={closeCart}>
-            <ShoppingCart/>
-          </button>
         </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+};
