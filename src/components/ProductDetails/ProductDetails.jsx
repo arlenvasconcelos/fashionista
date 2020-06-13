3b17e9e628f3333a641a3d0c71bebaf05eb9fcdf
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-06-13 09:46:27 -0300

    remove openCart on AddItem

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,104 +1,99 @@
-import React, {useState} from 'react';
-import {useDispatch} from 'react-redux'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import {openCart, addItem} from '../../store/actions/cart';
+import { openCart, addItem } from "../../store/actions/cart";
 
-import './ProductDetails.scss';
-import ProductPrice from '../ProductPrice/ProductPrice';
+import "./ProductDetails.scss";
+import ProductPrice from "../ProductPrice/ProductPrice";
 
-const ProductDetails = ({product}) => {
-
+const ProductDetails = ({ product }) => {
   const dispatch = useDispatch();
 
-  const [selectedSize, setSelectedSize] = useState('');
+  const [selectedSize, setSelectedSize] = useState("");
   const [error, setError] = useState({
-    message: '',
-    status: false
+    message: "",
+    status: false,
   });
 
   const handleSize = (value) => {
     if (value === selectedSize) {
-      setSelectedSize('');
-    }
-    else {
+      setSelectedSize("");
+    } else {
       setSelectedSize(value);
       setError({
         message: "",
         status: false,
-      })
+      });
     }
-  }
+  };
 
   const handleAddItem = (e) => {
     e.preventDefault();
 
-    if (selectedSize !== ''){
+    if (selectedSize !== "") {
+      const { sizes, ...rest } = product;
 
-      const {sizes, ...rest} = product;
-      
-      dispatch(addItem({
-        ...rest,
-        selected_size: selectedSize,
-      }))
-      dispatch(openCart())
-    }
-    else {
+      dispatch(
+        addItem({
+          ...rest,
+          selected_size: selectedSize,
+        })
+      );
+    } else {
       setError({
         message: "Por favor, selecione um tamanho.",
         status: true,
-      })
+      });
     }
-
-  }
+  };
 
   return (
     <div className="product-details">
       <div className="details__content">
-        <h4 className="details__name">
-          {product.name}
-        </h4>
-        <p className="details__style">
-          Ref:{product.code_color}
-        </p>
+        <h4 className="details__name">{product.name}</h4>
+        <p className="details__style">Ref:{product.code_color}</p>
         <p className="details__color">
-          Cor: {product.color ? product.color.charAt(0)+ product.color.slice(1).toLowerCase() : '' }
+          Cor:{" "}
+          {product.color
+            ? product.color.charAt(0) + product.color.slice(1).toLowerCase()
+            : ""}
         </p>
         <div className="details__sizes">
-          {
-            product.sizes && product.sizes.map((item, key) => (
-              <button 
-                key={key} 
+          {product.sizes &&
+            product.sizes.map((item, key) => (
+              <button
+                key={key}
                 disabled={!item.available}
                 onClick={() => handleSize(item.size)}
-                className={selectedSize === item.size ? `sizes__item selected` : `sizes__item`}
+                className={
+                  selectedSize === item.size
+                    ? `sizes__item selected`
+                    : `sizes__item`
+                }
               >
                 {item.size}
-              </button> 
-            ))
-          }
+              </button>
+            ))}
         </div>
-        <ProductPrice 
+        <ProductPrice
           regularPrice={product.regular_price}
           actualPrice={product.actual_price}
           onSale={product.on_sale}
         />
         <p className="details__installments">
-          {product.installments ? `${product.installments} sem juros` : ''} 
+          {product.installments ? `${product.installments} sem juros` : ""}
         </p>
         <div className="details__button">
-          <button
-            onClick={handleAddItem}
-          >
-            COMPRAR
-          </button>
-          {
-            error.status ? <span className="details__button-error">{error.message}</span> :  <></>
-          }
+          <button onClick={handleAddItem}>COMPRAR</button>
+          {error.status ? (
+            <span className="details__button-error">{error.message}</span>
+          ) : (
+            <></>
+          )}
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetails;
